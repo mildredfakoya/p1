279df4621bce0b503940a706fc0735a446f0fa1f
Mildred Fakoya <luvlyayee@gmail.com>
2017-09-12 04:43:00 -0400

    Added folders and files for my page

---
+++ index.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang ="en">
+<head>
+  <title>Mildred Fakoya</title>
+<meta charset ="utf-8">
+<meta viewport = "width=device-width,initial-scale =1.0">
+<link rel ="stylesheet" type ="text/css" href ="css/style.css">
+</head>
+<body>
+<div class ="textcontainer">
+<h1>About Mildred Fakoya</h1>
+<img src ="images/me.jpg" alt ="Picture of Mildred Fakoya" class ="imgright">
+<p>My name is Mildred Ayeesha Fakoya. I am 30 Years Old. I live in Dominica Caribbean Island. I am a Nigerian by Birth.<br/>
+I have been married for over 7 years now. I have two sons. (6 years and 5 years old) </p>
+<p>I hold a BSc in Computer Science from Kogi State University, Nigeria. <br/>
+As a child I dreamt of being a Medical Doctor. I thought at that time that was my desire. After I graduated from the University with my degree, I felt something was missing. Was it my unfulfiled passion for wanting to be a doctor, or i just wanted more.
+</p>
+
+<h2>Self Exploration</h2>
+<p>1 year after Graduation, I was employed at a bank as a relationship officer. I spent 1 year at this job and resigned because i felt no sense of fulfillment.<br/>Long story short, I decided to put my degree to use. I started some self trainning in programming. I started with Visual Basic and C then JAVA, MySQl, HTML, CSS, JavaScript, PHP and some few others. I started to feel Fulfilled but there was something more.<br/>
+Then I met my husband and that was it. I wanted to be a developer for Health Information Systems. The realization of this gave me the type of fulfillment that i have been longing for.
+So i took the path to certifications form Microsft and Oracle. I didn't like being trainned, all I always needed was textbooks and relevant softwares. Afters few years of self trainning, i took a couple of certification exams from both Microsoft and Oracle and i passed them all.
+I felt i have proved a point to myself that i can now start on the path to my dream. </p>
+<h3>Path to Harvard Extension School</h3>
+I spent the next 6 months after my certification exams at a teaching hospital. I wanted and industrial attachment in Pathology to understand basically bacterial infections, the types of screenings used to detect them, the sensitivity to antibiotics and so on. i needed this knowledge for the first software i wanted to build. i gathered all the knowledgde i wanted and left after 6 months.
+then i spent 3 years after in an HIV/AIDS and tuberculosis center. it was a topic to explore. i wanted to get a full understanding og HIV from Screening - Reactive -Positive - Management - Death. i also wanted to understand all the OIs that comes with it and a lot of medical details. i got more than enough knowledge to get to work.
+My first Software was to create a Patient Monitoring, Management and Tracking Software that will enforce compliance and adherence and help in eradictation of the disease.
+my first demo of the software landed me a Consultant position for Case-Based survelliance system at Organization of Eastern Caribbean State. To customize the software for all 6 countries of the OECS under the Global fund HIV/TB elimination project.
+Job completed, and accepted and said by the stakeholders to be the best system they have ever seen.
+But still i really have not been trainned by anyone. so i saw this Introduction to computer science course from Harvard on EdX, i tried it, i loved the way the instructors teach and then i decided to get a graduate degree in Software Engineering from Harvard. So i am here and i love it so far. 
+</div>
+</body>
+</html>
